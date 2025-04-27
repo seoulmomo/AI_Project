b452225fe0603d028af22d24196a66e224e6fa02
momo <rhwoah7190@gmail.com>
2025-04-27 23:19:41 +0900

    # 모델 별 컴포넌트 분류

--- my-aiproject/src/app/page.tsx
+++ my-aiproject/src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
+import Chat from "./components/Chat";
 import ImageGenerator from "./components/ImageGenerator";
+import { useModelStore } from "@/store/modelStore";
 
 export default function Home() {
+  const { selectedModel } = useModelStore();
   return (
     <div>
       <main className="flex flex-col items-center justify-center h-screen">
-        <ImageGenerator />
+        {selectedModel === "dall-e-3" ? <ImageGenerator /> : <Chat />}
       </main>
     </div>
   );
